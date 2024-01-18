05a1829832f3320f0a840da6cdc4d16985bb513b
IonAlexa <Ion_Alexandra@student.ceu.edu>
2024-01-18 13:22:32 +0000

    Added first teo intro to python scripts

---
+++ 02_basic_python.py
@@ -0,0 +1,76 @@
+#Add numbers
+print(2+4)
+
+
+
+#Divide two numbers
+print(10/5)
+
+#Division returns a float
+print(type(10/5))
+
+#Exponential
+print(2**3)
+
+#Assigning variables
+x=5
+y=x**2
+print(y)
+
+#String
+a = "Hello how're you"
+print(a*5)
+
+b = "I'm good, thank you"
+print(a + " " + b)
+
+
+#print(a-b)
+
+#print(a:b)
+
+print(a[-1])
+
+print(a[0:5])
+
+
+print(len(a))
+
+
+#print(f 'Our string a has {len(a)} characters')
+
+
+print(2 == 2) #if you have a yes or no question than just ==
+
+print(2 == 3)
+
+print(True==1) #True has the value of 1
+
+print (True + True + True)
+
+#Check equivalence of two variables
+#print(a = b)
+
+print(2==2 and 3==3) # True only if both are true
+
+
+print(2=2 or 3=4) #True if only one is true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
